2daaca5a79761bc6f2c19834d1785013e7ff4802
lauthj <30321543+lauthj@users.noreply.github.com>
2017-10-25 08:47:10 -0500

    Add files via upload

--- ODBC_demo_plugin.py
+++ ODBC_demo_plugin.py
@@ -9,8 +9,8 @@ import logging
 from ruxit.api.base_plugin import BasePlugin
 from ruxit.api.snapshot import pgi_name
 
-class DemoPlugin(BasePlugin):
+class ODBCDemoPlugin(BasePlugin):
     def query(self, **kwargs):
-        pgi = self.find_single_process_group(pgi_name('C:\\users.lauthjo.workspace.ODBC_Python_Demo_Plugin.odbc_python_demo'))
+        pgi = self.find_single_process_group(pgi_name('ODBC_Python_Demo_Plugin.odbc_python_demo'))
         pgi_id = pgi.group_instance_id
-        self.results_builder.absolute(key='value', value=pgi.get_values(), entity_id=pgi_id)
+        self.results_builder.absolute(key='value', value=pgi_id, entity_id=pgi_id)
